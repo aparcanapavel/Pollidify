bfea740c1947096642c8bc0d164e9a5d294e73eb
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-11-25 14:29:06 -0800

    Fix minor issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from '../app';
 import Axios from 'axios';
 
 ReactDOM.render(<App />, document.getElementById('root'));
