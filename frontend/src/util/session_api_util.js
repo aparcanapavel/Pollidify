984e2c80357daf4055e0ff242d5f3aed7888b768
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-11-25 15:28:23 -0800

    Add AuthToken util

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -7,3 +7,11 @@ export const signup = (userData) => {
 export const login = (userData) => {
   return Axios.post('/api/users/login', userData);
 }
+
+export const setAuthToken = token => {
+  if(token) {
+    Axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    delete Axios.defaults.headers.common["Authorization"];
+  }
+}
