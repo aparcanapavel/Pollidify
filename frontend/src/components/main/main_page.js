e94d47949f4f44babae4e99c068bdca5989fda1f
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-11-25 15:55:44 -0800

    Add main page and nav components

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>A Yik Yak Clone/Hybrid</h1>
+        <footer>
+          Time to watch the world burn.
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
