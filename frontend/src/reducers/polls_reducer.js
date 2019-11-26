b1cba7fc6c18f18327cf2941f24893d2c41f159f
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-11-26 15:38:20 -0800

    Create polls reducer

---
+++ frontend/src/reducers/polls_reducer.js
@@ -0,0 +1,25 @@
+import {
+  RECEIVE_POLLS,
+  RECEIVE_POLL,
+  RECEIVE_USER_POLLS
+} from '../actions/poll_actions';
+
+const PollsReducer = (state = { all: {}, user: {}, new: undefined }, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+  switch(action.type) {
+    case RECEIVE_POLLS:
+      newState.all = action.polls.data;
+      return newState;
+    case RECEIVE_USER_POLLS:
+      newState.user = action.polls.data;
+      return newState;
+    case RECEIVE_POLL:
+      newState.new = action.poll.data;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default PollsReducer;
