c1842a7a3ea084c07a435f437e9c864594cff058
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-11-26 16:05:58 -0800

    Remove unused imports

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,11 +3,9 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
 import LandingPage from './landing_page';
 import './app.css';
-import { throws } from 'assert';
+
 
 class App extends React.Component{
   constructor(props) {
