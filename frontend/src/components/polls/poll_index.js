da36c162a1560fa209ce71501b82975cf6112a53
asaf <asafmohammad5@gmail.com>
2019-11-27 11:45:10 -0600

    Fix poll index issues

--- frontend/src/components/polls/poll_index.js
+++ frontend/src/components/polls/poll_index.js
@@ -8,24 +8,25 @@ class PollIndex extends React.Component {
 
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchPolls();
   }
 
   render() {
-    if (this.props.polls.length === 0) {
+    debugger
+    if (!this.props.polls) {
       return null
-    } else {
+    } 
       return (
         <div>
           <h2>All Polls</h2>
           {this.props.polls.map(poll => (
-            <div> <Link to={`/polls/${poll.id}`}>{poll.question}</Link> </div>
+            <div> <Link to={`/polls/${poll._id}`}>{poll.question}</Link> </div>
           ))}
         </div>
       );
     }
   }
-}
+
 
 export default PollIndex;
