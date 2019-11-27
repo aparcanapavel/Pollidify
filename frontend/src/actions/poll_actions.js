ff9ef682306bb945361c577ea9592871c1401ac3
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-11-27 10:28:01 -0800

    Changed poll action constant

--- frontend/src/actions/poll_actions.js
+++ frontend/src/actions/poll_actions.js
@@ -2,7 +2,7 @@ import { getPolls, getPoll, getUserPolls, writePoll } from '../util/poll_api_uti
 
 export const RECEIVE_POLLS = "RECEIVE_POLLS";
 export const RECEIVE_USER_POLLS = "RECEIVE_USER_POLL";
-export const RECEIVE_POLL = "RECEIVE_NEW_POLL";
+export const RECEIVE_POLL = "RECEIVE_POLL";
 
 
 export const receivePolls = polls => ({
