002d87dd73e175e38b3bbbd9fde6891296054f64
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-11-28 14:14:54 -0800

    Create voted polls components

---
+++ frontend/src/components/polls/voted_polls.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PollShowContainer from './poll_show_container.js';
+
+class VotedPolls extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      polls: []
+    }
+  }
+
+  componentWillMount() {
+    console.log(this.props.currentUser.id)
+    this.props.fetchVotedPolls(this.props.currentUser.id);
+  }
+
+  componentWillReceiveProps(newState) {
+    this.setState({ polls: newState.polls });
+  }
+
+  render() {
+    if (this.state.polls.length === 0) {
+      return (
+        <div classname="no-user-polls">
+          <h2>You have no voted Polls</h2>
+        </div>
+      )
+    } else {
+      return (
+        <div className="voted-polls">
+          <h2>All of Your Voted Polls</h2>
+          <div className="voted-poll">
+            {this.state.polls.map(poll => (
+              <PollShowContainer key={poll._id} question={poll.question} poll={poll} inherited={true} />
+            ))}
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+export default VotedPolls;
---
+++ frontend/src/components/polls/voted_polls_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { fetchVotedPolls } from '../../actions/poll_actions';
+import VotedPolls from './voted_polls';
+
+const mapStateToProps = (state) => {
+  return {
+    polls: Object.values(state.entities.polls.voted),
+    currentUser: state.session.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchVotedPolls: id => dispatch(fetchVotedPolls(id))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(VotedPolls)
