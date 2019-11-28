d31850bea48bdd696254efd1a3f646c1759a5855
aparcanapavel <aparcanapavel@gmail.com>
2019-11-28 15:40:14 -0500

    Casting votes

--- frontend/src/components/choices/choices.js
+++ frontend/src/components/choices/choices.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
 class Choices extends React.Component {
+  constructor(props){
+    super(props);
+    this.castVote = this.castVote.bind(this);
+  }
+
+  castVote(choiceId){
+    // debugger
+    console.log(choiceId);
+    // this.props.createVote(choiceId);
+  }
 
   componentDidMount () {
-    this.props.fetchChoices(this.props.pollId)
+    this.props.fetchChoices(this.props.pollId).then(() => {
+      this.forceUpdate(); // for martin
+    });
   }
 
   render () {
     if (!this.props.choices) {
-      return null
+      this.forceUpdate();
+      return <h1>loading</h1>
     }
-    const pollChoices = this.props.choices.map(choice => <li className="choice-response">{choice.response}</li>)
+    const pollChoices = this.props.choices.map(choice => {
+      return <li 
+        key={choice._id} 
+        className="choice-response"
+        onClick={() => this.castVote(choice._id)}>
+          {choice.response}
+      </li>
+    });
 
 
     return (
