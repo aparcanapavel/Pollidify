4472c00e9c00567efdef2b64e5471e628550aaf1
aparcanapavel <aparcanapavel@gmail.com>
2019-11-29 11:58:31 -0500

    unest the errors

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import SessionErrorsReducer from "./session_errors_reducer";
-import PollFormErrorsReducer from './poll_form_errors_reducer';
+import ChoicesErrorsReducer from './choices_errors_reducers';
+import PollsErrorsReducer from './polls_errors_reducer';
 
 export default combineReducers({
   session: SessionErrorsReducer,
-  pollForm: PollFormErrorsReducer
+  poll: PollsErrorsReducer,
+  choices: ChoicesErrorsReducer
 });
--- frontend/src/reducers/poll_form_errors_reducer.js
+++
@@ -1,8 +0,0 @@
-import ChoicesErrorsReducer from './choices_errors_reducers';
-import PollsErrorsReducer from './polls_errors_reducer';
-import { combineReducers } from "redux";
-
-export default combineReducers({
-  polls: PollsErrorsReducer,
-  choices: ChoicesErrorsReducer
-})
