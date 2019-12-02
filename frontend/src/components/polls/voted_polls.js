d3531901acb990b6837d0530794d57bceaedd107
asaf <asafmohammad5@gmail.com>
2019-12-01 18:53:20 -0600

    Add css styling to voted polls

--- frontend/src/components/polls/voted_polls.js
+++ frontend/src/components/polls/voted_polls.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PollShowContainer from './poll_show_container.js';
+import { Link } from 'react-router-dom';
+import './voted_polls.css'
 
 class VotedPolls extends React.Component {
   constructor(props) {
@@ -23,17 +25,19 @@ class VotedPolls extends React.Component {
     if (this.props.polls.length === 0) {
       return (
         <div className="no-user-polls">
-          <h2>You have no voted Polls</h2>
+          <h2 className="voted-poll-h2">You have no voted Polls</h2>
         </div>
       )
     } else {
       return (
         <div className="voted-polls">
-          <h2>All of Your Voted Polls</h2>
-          <div className="voted-poll">
-            {this.props.polls.map(poll => (
-              <PollShowContainer key={poll._id} question={poll.question} poll={poll} inherited={true} noGraph={true} />
-            ))}
+          <h2 className="voted-poll-h2">All of Your Voted Polls</h2>
+          <div className="voted-poll-cont">
+            <ul className="voted-poll">
+              {this.props.polls.map(poll => (
+                <li className="poll-voted"><Link to={`/polls/${poll._id}`}><PollShowContainer key={poll._id} question={poll.question} poll={poll} inherited={true} noGraph={true} /> </Link> </li>
+              ))}
+            </ul>
           </div>
         </div>
       );
