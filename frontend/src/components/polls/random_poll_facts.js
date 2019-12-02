9e88aaae9c2b51e720174209e2e672b4facd9430
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2019-12-01 17:18:29 -0800

    Style poll index page

---
+++ frontend/src/components/polls/random_poll_facts.js
@@ -0,0 +1,18 @@
+export const FactsArr = [
+  "This sentence is false.",
+  "Polls are opinions, not behavior. Things can change. They often do.",
+  "Polls that regularly release their findings to the public — with information about survey procedures and question wordings — are usually pretty good.",
+  "Always look at the way poll questions are worded. That can make a huge difference.",
+  "The best way to look at polls is to look at several of them taken at about the same time asking similar questions.",
+  "Polls are not designed to be predictive.",
+  "Randomness is the the key to polling.",
+  "It’s often difficult to tell a good poll from a bad poll.",
+  "No single poll should ever be taken as authoritative.",
+  "Polls of specific subgroups can be tricky.",
+  "Polls with complex questions should be avoided.",
+  "A poll is not a quiz. Don't treat it like one.",
+  "A 50-50 result does not necessarily mean people are sharply divided over the topic at hand.",
+  "The first known example of an opinion poll was a local straw poll conducted by The Harrisburg Pennsylvanian in 1824, showing Andrew Jackson leading John Quincy Adams by 335 votes to 169 in the presidential race.",
+  "You should never use a poll as a means to a conclusion of any kind.",
+  "When in doubt, remember: 42."
+];
